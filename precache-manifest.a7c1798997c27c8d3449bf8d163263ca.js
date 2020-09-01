0f3876ce9ad88dcfd1dda2f240d47665c184d1c9
ghana7989 <ghana.chindukuri@gmail.com>
2020-09-01 21:27:00 +0530

    Updates

---
+++ precache-manifest.a7c1798997c27c8d3449bf8d163263ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f18e25e389cd6007b0e11a640fd57961",
+    "url": "/Pizzza-Joint/index.html"
+  },
+  {
+    "revision": "4e7724baa38acd6dc4ec",
+    "url": "/Pizzza-Joint/static/css/main.7882c667.chunk.css"
+  },
+  {
+    "revision": "0eae87af6422d7c7e29c",
+    "url": "/Pizzza-Joint/static/js/2.2975e07b.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/Pizzza-Joint/static/js/2.2975e07b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e7724baa38acd6dc4ec",
+    "url": "/Pizzza-Joint/static/js/main.66ef5a75.chunk.js"
+  },
+  {
+    "revision": "07fef0a6be605ac9b9be",
+    "url": "/Pizzza-Joint/static/js/runtime-main.9d7d975a.js"
+  }
+]);
